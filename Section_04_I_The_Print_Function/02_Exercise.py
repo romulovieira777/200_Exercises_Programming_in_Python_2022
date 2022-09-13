8633eed17ddb7026448b5f4bbfe98c9970f854d6
Romulo Vieira <romulo.vieira777@gmail.com>
2022-09-13 12:27:17 -0300

    I. The print() Function

---
+++ Section_04_I_The_Print_Function/02_Exercise.py
@@ -0,0 +1,13 @@
+"""
+Exercise No. 02
+
+Assign to the variable age number 20. Using the age variable and the print() function print to the console the following
+text:
+    'I am 20 years old.'
+"""
+age = 20
+
+print("I am " + str(age) + " years old.")
+
+# another method
+print(f'I am {age} years old.')
---
+++ Section_04_I_The_Print_Function/03_Exercise.py
@@ -0,0 +1,18 @@
+"""
+Exercise No. 03
+
+Create two variables (you can freely choose names) and assign to them following values:
+    - 'Python'
+    - '3.8'
+
+Using these variables and the print() function, print to the console the following text:
+
+    'I am learning Python version 3.8'
+"""
+language = 'Python'
+version = '3.8'
+
+print(f'I am learning {language} version {version}')
+
+# # another method
+print('I am learning ' + language + ' version ' + version)
