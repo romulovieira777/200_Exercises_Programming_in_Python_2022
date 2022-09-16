601c26863b86cbf9044d29294b7e683793f3ebac
Romulo Vieira <romulo.vieira777@gmail.com>
2022-09-16 10:32:28 -0300

    IV. Data Types

---
+++ Section_07_IV_Data_Types/02_Exercise.py
@@ -0,0 +1,31 @@
+"""
+Exercise No. 02
+
+The following variables are given:
+
+    var1 = None
+    var2 = False
+    var3 = 'True'
+
+Print each type of variable on a separate line to the console as shown below:
+
+Expected result:
+
+    <class 'NoneType'>
+    <class 'bool'>
+    <class 'str'>
+"""
+# Solution I
+var = [None, False, 'True']
+
+for i in var:
+    print(type(i))
+
+# Solution II
+var1 = None
+var2 = False
+var3 = 'True'
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
