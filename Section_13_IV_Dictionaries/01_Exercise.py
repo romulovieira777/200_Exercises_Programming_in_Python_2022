0ec2e3c1a58e6e87a667b830a3dc4973d4a69355
Romulo Vieira <romulo.vieira777@gmail.com>
2022-09-22 09:26:00 -0300

    IV. Dictionaries

---
+++ Section_13_IV_Dictionaries/01_Exercise.py
@@ -0,0 +1,17 @@
+"""
+Exercise No. 01
+
+Create a dictionary from the following pairs (key, value):
+
+    'USA': 'Washington'
+    'Germany': 'Berlin'
+    'Austria': 'Vienna'
+
+and print it to the console.
+
+Expected result:
+
+    {'USA': 'Washington', 'Germany': 'Berlin', 'Austria': 'Vienna'}
+"""
+capitals = {'USA': 'Washington', 'Germany': 'Berlin', 'Austria': 'Vienna'}
+print(capitals)
