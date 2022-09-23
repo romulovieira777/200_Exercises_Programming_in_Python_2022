c65546d679e313b57fc0ce9904f29a0ee9819a3f
Romulo Vieira <romulo.vieira777@gmail.com>
2022-09-23 12:48:03 -0300

    IV. Dictionaries

---
+++ Section_13_IV_Dictionaries/05_Exercise.py
@@ -0,0 +1,15 @@
+"""
+Exercise No. 05
+
+The following dictionary is given:
+
+    capitals = {'USA': 'Washington', 'Germany': 'Berlin', 'Austria': 'Vienna'}
+
+Using the dic.get() method, extract the value for key 'Autria' and print it to the console.
+
+Expected result:
+
+    Vienna
+"""
+capitals = {'USA': 'Washington', 'Germany': 'Berlin', 'Austria': 'Vienna'}
+print(capitals.get('Austria'))
