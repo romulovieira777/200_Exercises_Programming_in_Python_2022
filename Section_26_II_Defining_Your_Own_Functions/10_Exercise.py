93e2f8ab025b54b64960756366462cd300dcfa6d
Romulo Vieira <romulo.vieira777@gmail.com>
2022-10-12 11:03:53 -0300

    II. Defining Your Own Functions

---
+++ Section_26_II_Defining_Your_Own_Functions/10_Exercise.py
@@ -0,0 +1,22 @@
+"""
+Exercise No. 10
+
+Implement a function is_distinct() to check if the list contains unique values.
+
+Example:
+
+    [IN]: is_distinct([1, 2, 3])
+    [OUT]: True
+
+    [IN]: is_distinct([1, 2, 3, 3])
+    [OUT]: False
+
+Note! You only need to define the function.
+"""
+
+
+def is_distinct(items):
+    return len(items) == len(set(items))
+
+
+print(is_distinct([1, 2, 3]))
